ef2954996a301a147fc1cbaef949bbfcb86dcdb9
basilexet <blenderexet@gmail.com>
2022-08-10 00:08:36 +0300

    сокращение, правка лишнего пробела

---
+++ math.py
@@ -0,0 +1,8 @@
+import math
+a=1**100
+b=2**16
+c=2-1000
+d=2+2*2
+e=60/3
+f=math.sqrt(144)
+print('a=',a,'\n''b=',b,'\n''c=',c,'\n''d=',d,'\n''e=',e,'\n''f=',f,'\n')
--- new.py
+++ new.py
@@ -1,4 +1,4 @@
-s=float(input('s'))
+s=float(input('s'))#
 g=float(input('g'))
 f=s**g
 print('результат',f)
